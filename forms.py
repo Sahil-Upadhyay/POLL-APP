5decb4ec2a310dcac4048579ff1186ffbd3fb634
Sahil Upadhyay <67515591+Sahil-Upadhyay@users.noreply.github.com>
2023-01-17 12:49:15 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -8,10 +8,14 @@ class PollAddForm(forms.ModelForm):
                               widget=forms.TextInput(attrs={'class': 'form-control'}))
     choice2 = forms.CharField(label='Choice 2', max_length=100, min_length=2,
                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    choice3 = forms.CharField(label='Choice 3', max_length=100, min_length=2,
+                              widget=forms.TextInput(attrs={'class': 'form-control'}))
+    choice4 = forms.CharField(label='Choice 4', max_length=100, min_length=2,
+                              widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Poll
-        fields = ['text', 'choice1', 'choice2']
+        fields = ['text', 'choice1', 'choice2', 'choice3', 'choice4']
         widgets = {
             'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'cols': 20}),
         }
